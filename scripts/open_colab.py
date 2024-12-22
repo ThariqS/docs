1d7e001d7cae2f3283b966eb731548f6899705d4
Thariq Shihipar <trq212@gmail.com>
2024-12-21 22:25:26 -0800

    big update

--- scripts/open_colab.py
+++ scripts/open_colab.py
@@ -1,18 +1,21 @@
-import requests
-import webbrowser
-import json
 import base64
+import webbrowser
+
+import requests
+
 
 def open_notebook_in_colab(url):
     # Download the notebook content
     response = requests.get(url)
     notebook_content = response.text
-    
+
     # Encode the notebook content
     encoded_content = base64.b64encode(notebook_content.encode()).decode()
-    
+
     # Create a data URL
-    colab_url = f"https://colab.research.google.com/notebook#create=true&data={encoded_content}"
+    colab_url = (
+        f"https://colab.research.google.com/notebook#create=true&data={encoded_content}"
+    )
     webbrowser.open(colab_url)
 
 
